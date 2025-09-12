4a3b364258c100e56c7d906ea5ccb4d96f8b22e4
Yslee-dev-01 <ysleedev01@gmail.com>
2025-09-12 13:48:06 +0900

    Study: 프로젝트 안내 멘트 추가

--- SWIFT_Testing/SWIFT_Testing/SWIFT_TestingApp.swift
+++ SWIFT_Testing/SWIFT_Testing/SWIFT_TestingApp.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+/// 본 앱은 SwiftTesting을 학습하기 위한 프로젝트 입니다.
+/// SWIFT_TestingTests > SWIFT_TestingTests.swift 파일에 학습 내용이 적혀있습니다.
+
 @main
 struct SWIFT_TestingApp: App {
     var body: some Scene {
